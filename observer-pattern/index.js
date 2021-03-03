5b7d8e0f2f840f966cb37500b73e5baa52dee2c9
snowgogogo <snowgogogo@icloud.com>
2021-03-03 16:26:16 +0800

    update

--- observer-pattern/index.js
+++ observer-pattern/index.js
@@ -1,63 +1,34 @@
-function Subject() {
+function Subject(ctx) {
   // 观察者列表
   this.observers = [];
+  this._ctx = ctx || this;
 }
 
 subProto = Subject.prototype;
 
 // 添加一个观察者
-subProto.addObserver = function(observer) {
-  this.observers.push(observer);
+subProto.addObserver = function(fn) {
+  this.observers.push(fn);
+  return this;
 }
 
 // 删除一个观察者
-subProto.removeObserver = function(observerId) {
-  var observerIndex = '';
-  var delObserver = {};
-  this.observers.forEach(function(item, index) {
-    if (item.id === observerId) {
-      observerIndex = index;
-      delObserver = item;
+subProto.removeObserver = function(fn) {
+  this.observers = this.observers.filter(function(item) {
+    if (item !== fn) {
+      return item;
     }
   });
-  if (observerIndex !== '') {
-    this.observers.splice(observerIndex, 1);
-    return {
-      flag: 1,
-      delObserver: delObserver
-    };
-  } else {
-    // 未找到对应观察者
-    return {
-      flag: -1
-    };
-  }
+  return this;
 }
 
 // 通知
 subProto.notify = function(data) {
-  if (this.observers.length > 0) {
-    this.observers.forEach(function(item) {
-      item.update(data);
+  var observers = this.observers;
+  if (observers.length > 0) {
+    observers.forEach(function(item) {
+      item.call(this.ctx, data);
     });
-  } else {
-    return -1;
   }
-}
-
-function Observer(name, val) {
-  var obj = {};
-  obj.id = getId();
-  obj.name = name;
-  obj.val = val;
-  // 更新观察者
-  obj.update = function(data) {
-    this.val = data;
-  }
-  return obj;
-}
-
-// 生成一个随机数id
-function getId() {
-  return Math.random().toString(36).substring(2, 15);
+  return this;
 }
